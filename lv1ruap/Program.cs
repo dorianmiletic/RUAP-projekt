e27bcfeef23eee45059c3e704493765a94646ba1
dorian.miletic <dorian.miletic5@gmail.com>
2024-11-05 11:44:55 +0100

    Pročetno kreiranje aplikacije

---
+++ lv1ruap/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lv1ruap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Doriana Miletića!");
+        }
+    }
+}
